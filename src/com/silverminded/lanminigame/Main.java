258ac61a9d0ea1b7f1038ece7b01de43cd674636
Testyflesty <unconfigured@null.spigotmc.org>
2015-04-19 14:47:40 +0200

    Main updated

--- src/com/silverminded/lanminigame/Main.java
+++ src/com/silverminded/lanminigame/Main.java
@@ -1,9 +1,20 @@
 package com.silverminded.lanminigame;
 
+import net.amigocraft.mglib.api.Minigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by J0QUE on 19/04/2015.
  */
 
-public class Main {
+public class Main extends JavaPlugin{
+	//Maak een object voor een Minigame (Static zodat je hem overal kan callen)
+	public static Minigame mg;
+	
+	public void onEnable(){
+		//Registreer de plugin als een Minigame
+		mg = Minigame.registerPlugin(this);
+	}
 
 }
